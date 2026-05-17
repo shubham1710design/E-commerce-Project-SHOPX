7f46f1b2148cc8fb9dc743e147a3fc8b9293a014
Shubham Kumar <shubhamkr.997372@gmail.com>
2026-05-17 18:14:59 +0530

    server.js update

--- backend/server.js
+++ backend/server.js
@@ -33,6 +33,5 @@ app.get("/api/test", (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-  console.log("URI:", process.env.MONGO_URI);
 });
 
