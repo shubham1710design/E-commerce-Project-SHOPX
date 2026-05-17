aec17665128eddc7337ed8efe8389c716a365bd4
Shubham Kumar <shubhamkr.997372@gmail.com>
2026-05-17 22:07:18 +0530

    backend api updated

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,4 +1,4 @@
-const API_BASE_URL ="http://localhost:5000";
+const API_BASE_URL ="https://e-commerce-project-shopx.onrender.com";
 
 let allProducts = [];
 let wishlist = JSON.parse(localStorage.getItem("wishlist")) || [];
